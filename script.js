dbcab8209aa9ea759ab959733582641a4ef15ad1
Arun Vishwakarma <arunvishwakarma1307@gmail.com>
2026-07-10 21:43:19 +0530

    Added 3D hover effect

--- script.js
+++ script.js
@@ -25,3 +25,44 @@ function copyText(text) {
         toast.style.display = "none";
     }, 1500);
 }
+
+// 3D Card Effect
+
+document.querySelectorAll(".command").forEach(card => {
+
+    card.addEventListener("mousemove", (e) => {
+
+        const button = e.target.closest("button");
+
+        // agar mouse button ke upar hai to effect band
+        if(button){
+            return;
+        }
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = ((y - centerY) / centerY) * -8;
+        const rotateY = ((x - centerX) / centerX) * 8;
+
+        card.style.transform =
+        `perspective(800px)
+        rotateX(${rotateX}deg)
+        rotateY(${rotateY}deg)
+        translateY(-5px)`;
+    });
+
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform =
+        "perspective(800px) rotateX(0) rotateY(0) translateY(0)";
+
+    });
+
+});
